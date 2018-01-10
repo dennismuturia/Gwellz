db67599a1930624de74e1cf00b8e3b7de3d4a55d
dennismuturia <dmuturia2922@gmail.com>
2018-01-10 16:29:24 +0300

    Successfully integrated Bootstrap 4 in the project. Also will look into adding more bootstrap frameworks into the app. As well as custom css. The master Css is just a place holder for the css

--- Gwellz/views.py
+++ Gwellz/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+# We no longer need this
+# from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
