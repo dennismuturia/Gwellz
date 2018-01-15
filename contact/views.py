8a8181d8f4783d9d7d321d123f1729e7e50b863f
dennismuturia <dmuturia2922@gmail.com>
2018-01-15 15:00:56 +0300

    Modifying the views.py file to make it able to send emails from the site to specific mails

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+#This is the piece of code for sending the EmailField
+from django.core.mail import send_mail
+from django.conf import settings
+
 from .forms import contactForms
 
 # Create your views here.
 def contact(request):
     form = contactForms(request.POST or None)
-
+    subject = ''
+    message = ''
+    email = ''
+    send_mail(
+        'Subject here',
+        'Here is the message.',
+        'from@example.com',
+        ['to@example.com'],
+        fail_silently=False,
+    )
     if form.is_valid:
         print (request.POST)
     return render(request, 'contact.html', locals())
