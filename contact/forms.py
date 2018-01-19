736179db24ecdb011fd6208bef316174205f9f2d
dennismuturia <dmuturia2922@gmail.com>
2018-01-19 17:46:30 +0300

    JUst editing the styling of the contact us page. Planning of adding a background image on the site.

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class contactForms(forms.Form):
-    name = forms.CharField(required=False, max_length=100, help_text='Please enter your name')
-    email = forms.EmailField(required=True)
-    comment = forms.CharField(required=True, widget=forms.Textarea)
+    name = forms.CharField(required=False, max_length=100, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'What does your mom call you?'}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Where can we email you back'}))
+    comment = forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Things in your mind?'}))
