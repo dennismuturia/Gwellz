eea1ac88a4b38ab9da8e1fb2d2eaf97a4721a277
dennismuturia <dmuturia2922@gmail.com>
2018-01-23 09:27:59 +0300

    In these files, I have imported the authentication modile from django contrib that will be used to initialize the projects authentication.

--- Company/settings.py
+++ Company/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'fontawesome',
     'contact',
     'projects',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import(
+    authenticate,
+    get_user_model,
+    login,
+    logout
+)
 from django.shortcuts import render
-
 # Create your views here.
+
+def login_view(request):
+    return render(request, 'user.html', {})
+
+def register_view(request):
+    return render(request, 'user.html', {})
+
+def logout_view(request):
+    return render(request, 'user.html', {})
